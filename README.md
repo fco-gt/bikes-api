e47729eaa80fc32a1f81d9b520202b3b7c77ae03
Francisco <74993776+fco-gt@users.noreply.github.com>
2024-05-05 15:10:38 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Bikes API
+
+API que sirve para un catalogo de bicicletas
+
+# Tecnologias usadas:
+- Node.js
+- Javascript
+
+## Importante
+Este catalogo es inventado, toda la inforamcion es inventada y no tengo propiedad sobre ninguna de las imagenes utilizadas. Favor de usar con precaucion.
---
+++ index.js
@@ -0,0 +1,43 @@
+// Express
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.json());
+const port = 3001 || process.env.PORT;
+
+// Files
+const bikes = require("./products/bikes");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/bikes", (req, res) => {
+  const bikesWithImages = bikes.map((bike) => {
+    return {
+      ...bike,
+      imageUrl: `http://localhost:${port}/bikes/${bike.id}/imagen`, // Configure the URL
+    };
+  });
+  res.json(bikesWithImages);
+});
+
+app.get("/bikes/:id/imagen", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const bike = bikes.find((bike) => bike.id === parseInt(id));
+  console.log(bike);
+  if (bike) {
+    const imagePath = path.join(
+      __dirname,
+      `products/imgs/bikes/${bike.id}.png`
+    );
+    res.sendFile(imagePath);
+  } else {
+    res.status(404).send("Bike not found");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
